e80469a2ded1a5c064b808ba6545ff31980dce18
RobertoMacaccaro <robertomacaccaro@mail.polimi.it>
2020-04-06 18:07:27 +0200

    Added coord class and coord test

---
+++ src/main/java/it/polimi/ingsw/PSP43/model/Coord.java
@@ -0,0 +1,28 @@
+package it.polimi.ingsw.PSP43.model;
+
+public class Coord {
+    private final int xPosition;
+    private final int yPosition;
+
+    /**
+     * Not default constructor to create coordinates
+     * @param x is the x coordinate
+     * @param y is the y coordinate
+     */
+    public Coord(int x, int y){
+        this.xPosition=x;
+        this.yPosition=y;
+    }
+
+    /**
+     * Method to return the x coordinate
+     * @return the x coordinate
+     */
+    public int getX(){return xPosition;}
+
+    /**
+     * Method to return the y coordinate
+     * @return the y coordinate
+     */
+    public int getY(){return yPosition;}
+}
