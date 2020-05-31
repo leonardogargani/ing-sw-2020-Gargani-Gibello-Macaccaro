5da13d7dea441c7d54bd6130a4ae29c4629d9af3
RobertoMacaccaro <roberto.macaccaro@mail.polimi.it>
2020-05-31 17:22:00 +0200

    Fix error in end controller

--- src/main/java/it/polimi/ingsw/PSP43/client/gui/controllers/game_end/EndController.java
+++ src/main/java/it/polimi/ingsw/PSP43/client/gui/controllers/game_end/EndController.java
@@ -6,12 +6,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 public class EndController extends AbstractController {
     @FXML private ImageView homeButton;
     @FXML private Label cloudLabel;
     @FXML private Label homeButtonLabel;
     @FXML private ImageView centralImage;
+    @FXML private AnchorPane anchor;
+
+    private boolean buttonActive = true;
 
     @FXML
     private void initialize() {
@@ -24,26 +28,24 @@ public class EndController extends AbstractController {
      * This method handles homeButton, it restarts the game
      */
     public void onHomeButtonClicked() {
-        //this part will be in handle exit
-        /*ClientManager clientManager = new ClientManager(2,false);
-        Thread clientManagerThread = new Thread(clientManager);
-        clientManagerThread.start();*/
-        //Load home.fxml
-        super.handleExit();
+        if(homeButton.isVisible()){
+        super.handleExit();}
     }
 
     /**
      * This method shows the explication of the home button when you move on it
      */
     public void showHomeButtonLabel() {
-        homeButtonLabel.setText("Go back home and start a new match!");
+        if(homeButton.isVisible())
+            homeButtonLabel.setText("Go back home and start a new match!");
     }
 
     /**
      * This method hides the explication of the home button when you aren't on it
      */
     public void hideHomeButtonLabel() {
-        homeButtonLabel.setText("");
+        if(homeButton.isVisible())
+            homeButtonLabel.setText("");
     }
 
     /**
@@ -57,6 +59,14 @@ public class EndController extends AbstractController {
         else if(message.getEndGameHeader() == EndGameMessage.EndGameHeader.LOSER){
             centralImage.setImage(new Image(getClass().getResource("/images/others/loser.png").toExternalForm()));
             cloudLabel.setText("You lost the game!");
+        }
+        else if(message.getEndGameHeader() == EndGameMessage.EndGameHeader.SERVER_CRASHED){
+            homeButton.setVisible(false);
+            homeButton.setDisable(true);
+            homeButtonLabel.setVisible(false);
+
+            centralImage.setImage(new Image(getClass().getResource("/images/others/server_down.png").toExternalForm()));
+            cloudLabel.setText("The server is down!");
         }
         else{
             centralImage.setImage(new Image(getClass().getResource("/images/others/no_connection.png").toExternalForm()));
